8fb7b47ea793aff2beb96c1d9b2d190436c57fd3
Aurel Gjonaj <aurel.gjonaj.it@gmail.com>
2024-05-17 16:48:24 +0200

    init var

--- ExerciseCSharp/Program.cs
+++ ExerciseCSharp/Program.cs
@@ -4,11 +4,27 @@ namespace CSharpExercise
 {
     class Program
     {
-        static void Main(string[] args) // everything inside main will be executed // args = argoments
+        static void Main(string[] args) // everything inside main will be executed // args = arguments
         {
-            Console.WriteLine("Hello, World!"); //print hello, World!
-            Console.Read(); // used to make sure it dosen't close by himself, it doesen't close until you press a key
+
+            int numInt = 5; //initialized variable (variabile di tipo intero)
+            //float numFloat = 1.2F; //initialized variable (variabile di tipo Numeri con virgola) //F is a suffix
+            //double numDouble = 4.5684; //initialized variable (variabile di tipo Numeri con virgola ma puo' avere piu' cifre dopo la virgola)
+            //bool trueBool = true; //initialized variable (variabile di tipo booleana true)
+            //bool falseBool = false; //initialized variable (variabile di tipo booleana false)
+            //char sentChar = 'Hello World!'; //initialized variable (variabile di tipo booleana false)
+            //string textString = "Hello World!"; //initialized variable (variabile di tipo booleana false)
+
+            Console.WriteLine(numInt); //print hello, World!
+
+            Console.Read(); // used to make sure it doesn't close by himself, it doesn't close until you press a key
         }
+
+        //static bool trueResponse(int numInt) // bool ritorna un valore booleano // (int numInt) -> valore di ingresso
+        //{
+        //    Console.WriteLine(numInt);
+
+        //}
 
     }
 }
