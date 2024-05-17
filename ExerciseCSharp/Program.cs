e01b3fd362b7c632f675aee32b31a88b60b4bdca
Aurel Gjonaj <aurel.gjonaj.it@gmail.com>
2024-05-17 15:45:50 +0200

    Console.Read

--- ExerciseCSharp/Program.cs
+++ ExerciseCSharp/Program.cs
@@ -1,13 +1,13 @@
-﻿using System;
+﻿using System; //using this mainspace
 
 namespace CSharpExercise
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // everything inside main will be executed // args = argoments
         {
-            Console.WriteLine("Hello, World!");
-            Console.Read();
+            Console.WriteLine("Hello, World!"); //print hello, World!
+            Console.Read(); // used to make sure it dosen't close by himself, it doesen't close until you press a key
         }
 
     }
