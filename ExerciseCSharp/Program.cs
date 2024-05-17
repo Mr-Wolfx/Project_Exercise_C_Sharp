de926a5ebe4afe39ee0415875f9036f0c73754b4
Aurel Gjonaj <aurel.gjonaj.it@gmail.com>
2024-05-17 15:37:10 +0200

    #Hello World!

---
+++ ExerciseCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Read();
+        }
+
+    }
+}
