d0c4be8f053085cbd9506dc0670fdbee045e2a4e
Aurel Gjonaj <aurel.gjonaj.it@gmail.com>
2024-05-20 10:48:00 +0200

    init var, declare var, assing var

--- ExerciseCSharp/Program.cs
+++ ExerciseCSharp/Program.cs
@@ -7,7 +7,43 @@ namespace CSharpExercise
         static void Main(string[] args) // everything inside main will be executed // args = arguments
         {
 
-            int numInt = 5; //initialized variable (variabile di tipo intero)
+            int numInt = 5; //initialized variable (variabile di tipo intero) (variabile dichiarata,variabile assegnata,variabile inizzializzata )
+
+            string name = "Jon"; //variabile inizzializzata
+            string surname;//variabile dichiarata
+            surname = "Smith"; //variabile assegnata
+
+            string apici = "20"; // un numero dentro "" e' una stringa
+
+            // variabile riassegnata
+            name = "Luke";
+            surname = "Jonson";
+
+            Console.WriteLine(name + " " + surname); // la variabile sara' riassegnata con successo e apparira' Luke Jonson
+
+
+            // Fizz buzz problem
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //if (i % 3 == 0 && i % 5 == 0)
+            //{
+            //Console.WriteLine("FizzBuzz");
+            //}
+            //else if (i % 3 == 0)
+            //{
+            //Console.WriteLine("Fizz");
+            //}
+            //else if (i % 5 == 0)
+            //{
+            //Console.WriteLine("Buzz");
+            //}
+            //else
+            //{
+            //Console.WriteLine(i);
+            //}
+            //}
+
+
             //float numFloat = 1.2F; //initialized variable (variabile di tipo Numeri con virgola) //F is a suffix
             //double numDouble = 4.5684; //initialized variable (variabile di tipo Numeri con virgola ma puo' avere piu' cifre dopo la virgola)
             //bool trueBool = true; //initialized variable (variabile di tipo booleana true)
@@ -15,7 +51,7 @@ namespace CSharpExercise
             //char sentChar = 'Hello World!'; //initialized variable (variabile di tipo booleana false)
             //string textString = "Hello World!"; //initialized variable (variabile di tipo booleana false)
 
-            Console.WriteLine(numInt); //print hello, World!
+            //Console.WriteLine(numInt); //print hello, World!
 
             Console.Read(); // used to make sure it doesn't close by himself, it doesn't close until you press a key
         }
--- ExerciseCSharp/obj/Debug/net8.0/ExerciseCSharp.AssemblyInfo.cs
+++ ExerciseCSharp/obj/Debug/net8.0/ExerciseCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExerciseCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e01b3fd362b7c632f675aee32b31a88b60b4bdca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fb7b47ea793aff2beb96c1d9b2d190436c57fd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExerciseCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExerciseCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
